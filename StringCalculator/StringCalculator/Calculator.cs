6b907c138a5c811d605cd71a976d38b48c61c3e9
MonikaSla <103637350+MonikaSla@users.noreply.github.com>
2022-11-30 15:55:30 +0000

    Pushing a folder

---
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public static class Calculator
+    {
+        // <summary>
+        // returns the sum of a delimiter seperated string of numbers
+        // </summary>
+        // <param name="numbers">delimiter seperated string of numbers</param>
+        // <returns>sum of numbers</returns>
+
+        public static int Add(string numbers)
+        {
+            int total = 0;
+
+            for (int i = 0; i < StringHelpers.StringToInts(numbers).Count(); i++)
+            {
+                total = total + StringHelpers.StringToInts(numbers).ElementAt(i);
+            }
+            return total;
+        }
+
+        //    public static string ChangeDefaultDelimiter(string input)
+        //    string customDelimiterIndicator = "//";
+        //    string newline = "\n";
+        //    {
+        //    string output = String.Empty;
+        //    if (input.StartsWith(customDelimiterIndicator))
+        //    string defaultDelimiter = input.Substring(customDelimiterIndicator.Length, input.IndexOf("\n") - customDelimiterIndicator.Length);
+        //    output = input.Replace(defaultDelimiter, ",");
+        //    int newlineIndex = output.IndexOf(newline);
+        //    output = output.Substring(newlineIndex, output.Length - newlineIndex);
+        //    return output;
+        //    }
+    }
+}
---
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,33 @@
+using StringCalculator;
+using System;
+using Xunit;
+
+namespace StringCalculatorTests
+{
+    public class StringCalculatorTests
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        [InlineData("1, 2", 3)]
+        public void Add_GivenSimpleString_ReturnsCorrectSum(string input, int expected)
+        {
+            Assert.Equal(expected, Calculator.Add(input));
+        }
+
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        public void Add_GivenNewLine_ReturnsCorrectSum(string input, int expected)
+        {
+            Assert.Equal(expected, Calculator.Add(input));
+        }
+
+        [Theory]
+        [InlineData("//;\n1;2", 3)]
+        public void Add_GivenDefinedDelimiter_ReturnsCorrectSum(string input, int expected)
+        {
+            Assert.Equal(expected, Calculator.Add(input));
+        }
+    }
+}
